eb68be8d8c2295847889a012a93fdf387af7b6c4
bc004644 <bill.call@broadcom.com>
2025-02-28 14:21:52 -0600

    start VLP Agent if in prod

--- labstartup.sh
+++ labstartup.sh
@@ -109,6 +109,18 @@ while true;do
    sleep 5
 done
 
+# start the VLP Agent in prod if not already running
+startagent=`ps -ef | grep VLPagent.sh | grep -v grep`
+if [ "${startagent}" = "" ];then
+   cloud=`/usr/bin/vmtoolsd --cmd "info-get guestinfo.ovfenv" 2>&1 | grep vlp_org_name | cut -f3 -d: | cut -f2 -d\\`
+   if [ "${cloud}" = "" ];then
+      echo "Dev environment. Not starting VLP Agent." >> ${logfile}
+   else
+      echo "Prod environment. Starting VLP Agent." >> ${logfile}
+      /home/holuser/Tools/VLPagent.sh &
+   fi
+fi
+
 startupstatus=${mcholroot}/startup_status.txt
 
 # if run with the labcheck argument, only pass on to labstartup.py and exit
